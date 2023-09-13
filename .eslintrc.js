d138f3bf2d4086b8f6f127b42ccc6659a244edbe
Hassan Bulega <hassanbulega@gmail.com>
2023-09-13 09:37:51 +0300

    fix lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,26 @@
 module.exports = {
-    env: {
-        commonjs: true,
-        es2021: true,
-        node: true,
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    indent: ["error", 4],
+    "linebreak-style": ["error", "unix"],
+    quotes: [2, "single", { avoidEscape: true, allowTemplateLiterals: true }],
+    semi: ["error", "never"],
+    "no-unused-vars": "off",
+  },
+  overrides: [
+    {
+      files: ["**/*.test.js"],
+      env: {
+        jest: true,
+      },
     },
-    extends: 'eslint:recommended',
-    parserOptions: {
-        ecmaVersion: 'latest',
-    },
-    rules: {
-        indent: ['error', 4],
-        'linebreak-style': ['error', 'unix'],
-        quotes: [2, 'single', { avoidEscape: true, allowTemplateLiterals: true }],
-        semi: ['error', 'never'],
-    },
-    overrides: [
-        {
-            files: ['**/*.test.js'],
-            env: {
-                jest: true,
-            },
-        },
-    ],
-}
+  ],
+};
